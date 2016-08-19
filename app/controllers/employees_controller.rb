83e1b1c4d9b5a269dfdab2586c2a6bef1c889c76
Mark Richardson <mark@markeban.com>
2016-08-18 21:02:37 -0700

    DRY up by api domain with dot_env

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,32 +1,32 @@
 class EmployeesController < ApplicationController
   
   def index
-    @employees = Unirest.get("http://localhost:3000/api/v1/employees.json").body
+    @employees = Unirest.get("#{ENV['DOMAIN']}/employees.json").body
   end
 
   def show
-    @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}.json").body
+    @employee = Unirest.get("#{ENV['DOMAIN']}/employees/#{params[:id]}.json").body
   end
 
   def new
   end
 
   def create
-    @employee = Unirest.post("http://localhost:3000/api/v1/employees.json", parameters: {first_name: params[:first_name], last_name: params[:last_name], email: params[:email], birthdate: params[:birthdate], ssn: params[:ssn]}).body
+    @employee = Unirest.post("#{ENV['DOMAIN']}/employees.json", parameters: {first_name: params[:first_name], last_name: params[:last_name], email: params[:email], birthdate: params[:birthdate], ssn: params[:ssn]}).body
     redirect_to "/employees/#{@employee['id']}"
   end
 
   def edit
-    @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}.json").body
+    @employee = Unirest.get("#{ENV['DOMAIN']}/employees/#{params[:id]}.json").body
   end
 
   def update
-    @employee = Unirest.patch("http://localhost:3000/api/v1/employees/#{params[:id]}.json", parameters: {first_name: params[:first_name], last_name: params[:last_name], email: params[:email], birthdate: params[:birthdate], ssn: params[:ssn]}).body
+    @employee = Unirest.patch("#{ENV['DOMAIN']}/employees/#{params[:id]}.json", parameters: {first_name: params[:first_name], last_name: params[:last_name], email: params[:email], birthdate: params[:birthdate], ssn: params[:ssn]}).body
     redirect_to "/employees/#{@employee['id']}"
   end
 
   def destroy
-    Unirest.delete("http://localhost:3000/api/v1/employees/#{params[:id]}.json").body
+    Unirest.delete("#{ENV['DOMAIN']}/employees/#{params[:id]}.json").body
     redirect_to "/employees"
   end
 
