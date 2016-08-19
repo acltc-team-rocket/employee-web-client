eee135a4e875f3664a521b8f50958c263bb0ad2b
Mark Richardson <mark@markeban.com>
2016-08-18 18:31:15 -0700

    added new and create action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,6 +1,18 @@
 class EmployeesController < ApplicationController
+  
+  def index
+    @employees = Unirest.get("http://localhost:3000/api/v1/employees.json").body
+  end
 
   def show
     @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}.json").body
+  end
+
+  def new
+  end
+
+  def create
+    @employee = Unirest.post("http://localhost:3000/api/v1/employees.json", parameters: {first_name: params[:first_name], last_name: params[:last_name], email: params[:email], birthdate: params[:birthdate], ssn: params[:ssn]}).body
+    redirect_to "/employees/#{@employee['id']}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/employees', to: 'employees#index'
+  get '/employees/new', to: 'employees#new'
   get '/employees/:id', to: 'employees#show'
+  post '/employees', to: 'employees#create'
 end
